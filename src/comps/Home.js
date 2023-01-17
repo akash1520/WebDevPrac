376b85186e4b1b6246111d9b54c0d48883ea7706
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-01-17 15:48:51 +0530

    learnt how to useReducer

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const ACTION={
+  INCREMENT:'increment',
+  DECREMENT:'decrement'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { age: state.age + 1 }
+    case ACTION.DECREMENT:
+      return {age:state.age-1}
+  }
+}
 
 export default function Home() {
+  const [state, dispatch] = useReducer(reducer, { age: 0 })
   return (
-    <div>Home</div>
+    <>
+      <button onClick={()=>{dispatch({type:ACTION.INCREMENT})}}>+</button>
+      {state.age}
+      <button onClick={()=>{dispatch({type:ACTION.DECREMENT})}}>-</button>
+    </>
   )
 }
