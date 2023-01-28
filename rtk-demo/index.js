3465fe5b3ca3f6f5748799e2665ec23ac13752e0
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-01-28 19:53:31 +0530

    learninf redux toolkit

---
+++ rtk-demo/index.js
@@ -0,0 +1,15 @@
+const store = require("./app/store");
+const cakeActions = require("./features/cake/cakeSlice").cakeActions
+const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions
+
+console.log(store.getState())
+const unsubscribe = store.subscribe(()=>{
+    console.log("updated state", store.getState())
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+
+unsubscribe()
